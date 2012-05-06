4f353e753c1cc34be881e032c25f4b67553b0c76
ongaeshi <ongaeshi0621@gmail.com>
2012-05-06 23:03:36 +0900

    Refectaring. 1. auto-shell-command: -> ascmd: 2. Divide public & private

--- auto-shell-command.el
+++ auto-shell-command.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012 ongaeshi
 
 ;; Author: ongaeshi
-;; Keywords: shell, save, async, deferred
+;; Keywords: shell, save, async, deferred, auto
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -20,80 +20,93 @@
 
 ;;; Commentary:
 
-;; 
+;; @todo
 
-;;; Code:
+;;; Samples:
 
-(eval-when-compile (require 'cl))
-(require 'deferred)
-
-;;; Parameter:
-
-(defun auto-shell-command:notify (msg)
-  (message msg)                                                        ; simple
-  ;;(deferred:process-shell (format "growlnotify -m %s -t emacs" msg)) ; Growl(OSX)
-  ;; (deferred:process-shell (format "growlnotify %s /t:emacs" msg))   ; Growl(Win)
-  )
-
-(defvar auto-shell-command:active t)
-
-(defvar auto-shell-command:buffer-name "*Auto Shell Command*")
-
-;;; Main:
-
-(defun auto-shell-command:shell-deferred (arg &optional notify-start)
-  (lexical-let ((arg arg)
-                (notify-start notify-start)
-                (result "success"))
-    (deferred:$
-      ;; before
-      (deferred:next
-        (lambda () (if notify-start (auto-shell-command:notify "start"))))
-      ;; main
-      (deferred:process-shell arg)
-      (deferred:error it (lambda (err) (setq result "failed") (pop-to-buffer auto-shell-command:buffer-name) err))
-      ;; after
-      (deferred:nextc it
-        (lambda (x)
-          (with-current-buffer (get-buffer-create auto-shell-command:buffer-name)
-            (delete-region (point-min) (point-max))
-            (insert x))
-          (auto-shell-command:notify result))))))
-
-; 自動コンパイルのON/OFF
-(defun auto-shell-command:toggle ()
-  (interactive)
-  (if auto-shell-command:active
-      (setq auto-shell-command:active nil)
-    (setq auto-shell-command:active t))
-  (message "auto-shell-command %s" auto-shell-command:active))
-
-;; @latertodo C-cC-mに割り当て(後で外すかも)
-(global-set-key "\C-c\C-m" 'auto-shell-command:toggle)
-
-;; Command list
-(setq auto-shell-command:setting nil)
-
-;; Add command
+;; @todo
 ;; ex.
+;;  書き直す
 ;;   (setq auto-shell-command:setting
 ;;         (("/path/to/dir/BBB/test" 'match-dir  "make test"                                ("cpp hpp"))
 ;;          ("/path/to/dir/doc"      'match-dir  "make all doc"                             ("html"))
 ;;          ("/path/to/dir/AAA"      'match-dir  "make all && (cd /path/to/dir/BBB/; make)")
 ;;          ("/path/to/dir/BBB"      'match-dir  "(cd /path/to/dir/AAA/; make) && make")
 ;;          ("/path/to/dir"          'buffer-dir "make")))
-(defun ascmd:add (v) (push v auto-shell-command:setting))
 
-; 一つのコマンドを実行
-(defun auto-shell-command:exec1 (path command)
+;;; Code:
+
+(eval-when-compile (require 'cl))
+(require 'deferred)
+
+(setq ascmd:version "0.1")
+
+;;; Public:
+
+;; Notify function
+(defun ascmd:notify (msg)
+  (message msg)                                                        ; emacs's message function
+  ;;(deferred:process-shell (format "growlnotify -m %s -t emacs" msg)) ; Growl(OSX)
+  ;;(deferred:process-shell (format "growlnotify %s /t:emacs" msg))   ; Growl(Win)
+  )
+
+;; Toggle after-save-hook (Recommended to set the key bindings)
+(defun ascmd:toggle ()
+  (interactive)
+  (if ascmd:active
+      (setq ascmd:active nil)
+    (setq ascmd:active t))
+  (message "auto-shell-command %s" ascmd:active))
+
+(defvar ascmd:active t)
+
+;; Add to command list
+(defun ascmd:add (v) (push v ascmd:setting))
+
+;; Result buffer name
+(defvar ascmd:buffer-name "*Auto Shell Command*")
+
+;;; Private:
+
+;; Command list
+(setq ascmd:setting nil)
+
+;; Exec-command when you save file
+(add-hook 'after-save-hook 'ascmd:exec)
+
+(defun ascmd:exec ()
+  (interactive)
+  (if ascmd:active
+      (find-if '(lambda (v) (apply 'ascmd:exec1 v)) ascmd:setting)))
+
+(defun ascmd:exec1 (path command)
   (if (string-match path (buffer-file-name))
       (progn
-        (auto-shell-command:shell-deferred (ascmd:query-reqplace command (buffer-file-name)))
-        ; (auto-shell-command:shell-deferred command t) ; notify-start
+        (ascmd:shell-deferred (ascmd:query-reqplace command (buffer-file-name)))
+        ; (ascmd:shell-deferred command t) ; notify-start
         t)
     nil))
 
-; query-replace special variable
+(defun ascmd:shell-deferred (arg &optional notify-start)
+  (lexical-let ((arg arg)
+                (notify-start notify-start)
+                (result "success"))
+    (deferred:$
+      ;; before
+      (deferred:next
+        (lambda () (if notify-start (ascmd:notify "start"))))
+      ;; main
+      (deferred:process-shell arg)
+      (deferred:error it (lambda (err) (setq result "failed") (pop-to-buffer ascmd:buffer-name) err))
+      ;; after
+      (deferred:nextc it
+        (lambda (x)
+          (with-current-buffer (get-buffer-create ascmd:buffer-name)
+            (delete-region (point-min) (point-max))
+            (insert x))
+          (ascmd:notify result))))))
+
+;; query-replace special variable
 (defun ascmd:query-reqplace (command match-path)
   (let (
         (file-name (file-name-nondirectory match-path))
@@ -103,15 +116,6 @@
     (setq command (replace-regexp-in-string "$FILE" file-name command t))
     (setq command (replace-regexp-in-string "$DIR" dir-name command t))
     command))
-
-; auto-shell-commandの実行
-(defun auto-shell-command:exec ()
-  (interactive)
-  (if auto-shell-command:active
-      (find-if '(lambda (v) (apply 'auto-shell-command:exec1 v)) auto-shell-command:setting)))
-
-; ファイルセーブ時にコンパイル
-(add-hook 'after-save-hook 'auto-shell-command:exec)
 
 (provide 'auto-shell-command)
 ;;; auto-shell-command.el ends here
