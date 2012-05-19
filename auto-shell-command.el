357d8a236ddddc014617a95b479649eede883b51
ongaeshi <ongaeshi0621@gmail.com>
2012-05-20 00:23:51 +0900

    for MELPA

--- auto-shell-command.el
+++ auto-shell-command.el
@@ -4,6 +4,7 @@
 
 ;; Author: ongaeshi
 ;; Keywords: shell, save, async, deferred, auto
+;; Version: 0.3.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -73,8 +74,6 @@
 
 (eval-when-compile (require 'cl))
 (require 'deferred)
-
-(setq ascmd:version "0.3.0")
 
 ;;; Public:
 
